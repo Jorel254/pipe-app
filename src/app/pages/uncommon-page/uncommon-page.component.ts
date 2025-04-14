64339d75ce36131a64dca1c4e652352c54c80d0c
Santiago González Santiesteban <gonzalez.s.santiago@hotmail.com>
2025-04-14 13:04:33 -0600

    Se agrega i18SelectPipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { I18nSelectPipe } from '@angular/common';
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
-  styleUrl: './uncommon-page.component.css'
+  styleUrl: './uncommon-page.component.css',
 })
 export class UncommonPageComponent {
+  client1 = {
+    name: 'Juan',
+    age: 20,
+    address: 'Mexico, CDMX',
+    gender: 'male',
+  };
+  client2 = {
+    name: 'Maria',
+    age: 25,
+    address: 'Mexico, CDMX',
+    gender: 'female',
+  };
 
+  client = signal(this.client1);
+
+  changeClient() {
+    if (this.client() === this.client1) {
+      this.client.set(this.client2);
+    } else {
+      this.client.set(this.client1);
+    }
+  }
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 }
