2fcc1b1dce5142c44a1e605cb0b628ea07672837
Santiago González Santiesteban <gonzalez.s.santiago@hotmail.com>
2025-04-14 15:40:02 -0600

    Se crea pipe personalizado y creacion de data para continuidad de pipes personalizados

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { ToogleCasePipe } from '../../pipes/toogle-case.pipe';
+import { CardComponent } from '../../components/card/card.component';
+import { heroes } from '../../constants/HeroData';
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToogleCasePipe, CardComponent],
   templateUrl: './custom-page.component.html',
-  styleUrl: './custom-page.component.css'
+  styleUrl: './custom-page.component.css',
 })
 export class CustomPageComponent {
+  toogleCase = signal(true);
 
+  changeToogleCase() {
+    this.toogleCase.update((value) => !value);
+  }
+
+  heroes = signal(heroes);
 }
